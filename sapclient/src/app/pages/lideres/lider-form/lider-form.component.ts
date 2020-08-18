69be81aadbd58852c169bb0729a4c8ba429ecc77
diegotesch <diego.tesch@gmail.com>
2020-08-18 14:29:25 -0300

    cadastro

--- sapclient/src/app/pages/lideres/lider-form/lider-form.component.ts
+++ sapclient/src/app/pages/lideres/lider-form/lider-form.component.ts
@@ -1,4 +1,11 @@
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+
+import { LiderService } from './../../../services/lider.service';
+import { Lider } from './../../../models/lider.model';
 
 @Component({
   selector: 'app-lider-form',
@@ -7,9 +14,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiderFormComponent implements OnInit {
 
-  constructor() { }
+    titulo: string = 'Cadastro de líder';
+    acaoAtual: string;
+    form: FormGroup;
+    formSubmetido: boolean = false;
+    @BlockUI() blockUI: NgBlockUI;
+
+  constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+      private formBuilder: FormBuilder,
+      private liderService: LiderService
+  ) { }
 
   ngOnInit(): void {
+      this.setAcaoAtual();
+      this.iniciarForm();
+  }
+
+  private setAcaoAtual() {
+    if(this.route.snapshot.url[0].path == 'novo')  {
+        this.titulo = 'Cadastro de Líder';
+        return;
+    }
+    this.titulo = 'Editando líder';
+  }
+
+  iniciarForm() {
+      this.form = this.formBuilder.group({
+          id: [null],
+          nome: [null, [Validators.required, Validators.minLength(3)]],
+          contato: [null]
+      })
+  }
+
+  enviarForm() {
+      this.formSubmetido = true;
+      if (!this.form.invalid) {
+          this.salvar();
+      }
+  }
+
+  salvar() {
+      this.blockUI.start();
+      const recurso = Object.assign(new Lider(), this.form.value);
+      this.liderService.salvar(recurso).pipe(
+          finalize(() => this.blockUI.stop())
+      ).subscribe(
+          recurso => console.log(recurso)
+      )
   }
 
 }
--- sapclient/src/app/services/lider.service.ts
+++ sapclient/src/app/services/lider.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { environment } from './../../environments/environment.prod';
 import { Lider } from './../models/lider.model';
@@ -20,5 +21,24 @@ export class LiderService {
 
   obterTodos(): Observable<any> {
     return this.http.get(`${this.api}`)
+  }
+
+  salvar(recurso: Lider): Observable<any> {
+      if (recurso.id) {
+          return this.atualizar(recurso);
+      }
+      return this.cadastrar(recurso);
+  }
+
+  private cadastrar(recurso: Lider): Observable<Lider> {
+      return this.http.post(`${this.api}`, recurso).pipe(
+          map(recurso => Object.assign(new Lider(), recurso))
+      );
+  }
+
+  private atualizar(recurso: Lider): Observable<Lider> {
+      return this.http.put(`${this.api}`, recurso).pipe(
+          map(recurso => Object.assign(new Lider(), recurso))
+      );
   }
 }
