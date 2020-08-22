d9e2ae8912a102fd1fd4514dd23eaee4b74a7b79
Elias Machado <eliasmachado173@gmail.com>
2020-08-22 18:30:33 -0300

    Codigo Base DashBord

--- sapclient/src/app/pages/dashboard/dashboard.component.ts
+++ sapclient/src/app/pages/dashboard/dashboard.component.ts
@@ -1,3 +1,12 @@
+import { LiderService } from './../../services/lider.service';
+import { SprintService } from './../../services/sprint.service';
+import { ProjetoService } from './../../services/projeto.service';
+
+import { OrdemServicoService } from './../../services/ordem-servico.service';
+import { SituacaoService } from './../../services/situacao.service';
+import { Observable } from 'rxjs';
+
+import { finalize, map } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
@@ -9,16 +18,25 @@ import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
 export class DashboardComponent implements OnInit {
 
-  titulo: string = 'Dashboard'
   @BlockUI() blockUI: NgBlockUI;
 
-  dashboard = [
-    { os: 'site2', status: 'Em andamento', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys'},
-    { os: 'site', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys'},
-    { os: 'site3', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys'},
-    { os: 'site4', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys'},
-    { os: 'site5', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys'}
-  ]
+  titulo: string = 'Dashboard'
+  listaOrdemServico$: Observable<any>;
+  listaOrdemServico: any = [];
+  situacoes: any = [];
+  projetos: any = [];
+  sprints: any = [];
+  lideres: any = [];
+
+
+
+  // dashboard = [
+  //   { os: 'site2', status: 'Em andamento', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys' },
+  //   { os: 'site', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys' },
+  //   { os: 'site3', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys' },
+  //   { os: 'site4', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys' },
+  //   { os: 'site5', status: 'Pausado', pf: '20', proxEntrega: '20/05', prazo: 'não', defeitoCliente: '1', defeitoInterno: '10', impedimento: 'não', revisor: 'Diego', testador: 'Ana', gerente: 'Chrys' }
+  // ]
 
   colunas: any[] = [
     { header: 'OS' },
@@ -35,9 +53,114 @@ export class DashboardComponent implements OnInit {
     { header: 'Ações' },
   ];
 
-  constructor() { }
+  constructor(
+    private ordemServicoService: OrdemServicoService,
+    private projetoService: ProjetoService,
+    private situacaoService: SituacaoService,
+    private liderService: LiderService,
+    private sprintService: SprintService
+  ) { }
 
   ngOnInit(): void {
+
+    this.obterSituacoes();
+    this.obterProjetos();
+    this.obterTodos();
+    this.obterSprint();
+    this.obterLideres();
+
+
+  }
+  obterTodos() {
+
+    this.listaOrdemServico$ = this.ordemServicoService.obterTodos().pipe(
+      map(res => {
+        res.forEach(item => {
+          item.dataProximaEntrega = new Date(`${item.dataProximaEntrega}T00:00:00`);
+          item.prazo = new Date(`${item.prazo}T00:00:00`);
+          console.log(item.prazo);
+        })
+        return res;
+      }),
+      finalize(() => this.blockUI.stop())
+    )
+  }
+
+  deletar(id: number) {
+    this.blockUI.start();
+    this.ordemServicoService.deletar(id).pipe(
+      finalize(() => this.blockUI.stop())
+    ).subscribe(
+      () => this.obterTodos()
+    );
+  }
+
+
+
+  obterProjetos() {
+    this.blockUI.start();
+    this.projetoService.obterTodos().pipe(
+      finalize(() => this.blockUI.stop())
+    ).subscribe(
+      projetos => this.projetos = projetos
+    );
+  }
+
+
+  obterLideres() {
+    this.blockUI.start();
+
+    this.liderService.obterTodos().pipe(
+      finalize(() => this.blockUI.stop())
+    ).subscribe(
+      lideres => this.lideres = lideres
+    );
+  }
+  obterNomeLider(id: number) {
+    return this.lideres.find(lider => lider.id == id).nome
+  }
+
+  obterSprint() {
+    this.blockUI.start();
+    this.sprintService.obterTodos().pipe(
+      finalize(() => this.blockUI.stop())
+    ).subscribe(
+      sprints => this.sprints = sprints
+    );
+  }
+  obterSituacoes() {
+    this.blockUI.start();
+    this.situacaoService.obterTodos().pipe(
+      finalize(() => this.blockUI.stop())
+    ).subscribe(
+      situacoes => this.situacoes = situacoes
+    );
+  }
+  obterSituacaoSprint(id: number) {
+    return this.sprints.find(sprint => sprint.idOrdemServico == id).prazo
+  }
+
+  obterImpedimentoSprint(id: number) {
+    return this.sprints.find(sprint => sprint.idOrdemServico == id).impedimento
+  }
+
+  obterNomeSituacao(id: number) {
+    return this.situacoes.find(situacao => situacao.id == id).descricao
+  }
+
+  obterNomeProjeto(id: number) {
+    return this.projetos.find(projeto => projeto.id == id).nome
+  }
+
+  obterNomeGerente(id: number) {
+    return this.projetos.find(projeto => projeto.id == id).gerente
+  }
+  obterNomeTestador(id: number) {
+    return this.projetos.find(projeto => projeto.id == id).testador
+  }
+  obterNomeRevisor(id: number) {
+    return this.projetos.find(projeto => projeto.id == id).revisor
+
   }
 
 }
